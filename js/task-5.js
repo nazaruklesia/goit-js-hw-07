451707b6e9a737f3a66ee8d39a0dc744df2a6468
Lesia_Nazaruk <nazarullesia@gmail.com>
2024-11-02 18:42:31 +0200

    add file

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,19 @@
+
+const btn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const bodyColor = document.querySelector("body");
+
+btn.addEventListener("click", handleClick);
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function handleClick() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+  return;
 }
